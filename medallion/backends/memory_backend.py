7280e816297c3d11d702e8d462513aa90b350e9c
Jason Keirstead <Jason.Keirstead@ca.ibm.com>
2017-12-18 11:57:50 -0400

    - Update README to match actual configuration required in config.json - Add error handling messages for when things are not specified - Refactor to abstract away the Flask endpoint allowing medallion to be run as a library in an already-existing Flask application - Allow one to plug-in a backend python module without editing the source code by specifying it in config.json

--- medallion/backends/memory_backend.py
+++ medallion/backends/memory_backend.py
@@ -19,8 +19,13 @@ class MemoryBackend(Backend):
         self.data = {}
 
     def load_data_from_file(self, filename):
-        self.data = json.load(StringIO(open(filename, "r").read()))
-
+        import os.path
+        if os.path.isfile(filename) and os.path.getsize(filename) > 0: 
+            try:
+                self.data = json.load( StringIO( open(filename, "r").read() ) )
+            except:
+                raise ValueError("File " + filename + " contains invalid data")
+        
     def save_data_to_file(self, filename):
         with open(filename, 'w') as outfile:
             json.dump(self.data,
--- medallion/scripts/run.py
+++ medallion/scripts/run.py
@@ -1,20 +1,33 @@
 import json
 import sys
+import os.path
+import sys
 
-from medallion import (application_instance, get_config, init_backend,
-                       set_config)
+from flask import Flask, url_for, render_template
 
-
+from medallion import (init,register_blueprints)
 def main():
 
     if len(sys.argv) < 2:
-        raise ValueError("No config file")
+        raise ValueError("No config file specified")
+
     config_file_path = sys.argv[1]
-    with open(config_file_path, 'r') as f:
-        set_config(json.load(f))
+    if os.path.isfile(config_file_path) and os.path.getsize(config_file_path) > 0:
+        with open(config_file_path, 'r') as f:
+            config = None
+            try:
+                config = json.load(f)
+            except ValueError:
+                raise ValueError("File {0} contains invalid JSON".format(config_file_path))
+            init(config)
 
-    init_backend(get_config()['backend'])
+    else:
+        raise ValueError("File {0} is empty or not present".format(config_file_path))
 
+    application_instance = Flask(__name__)
+
+    register_blueprints(application_instance)
+    
     application_instance.run(debug=True)
 
 
