70956b51bf8fa28a4576c0bd35d63a501c13c75d
Emmanuelle Vargas-Gonzalez <emmanuelle@mitre.org>
2018-03-26 07:45:27 -0400

    Use proxy to register views and backend

--- medallion/__init__.py
+++ medallion/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-from flask import Flask
+from flask import Flask, current_app
 from flask_httpauth import HTTPBasicAuth
 
 from medallion.version import __version__  # noqa
@@ -45,20 +45,13 @@ def connect_to_backend(config_info):
             raise ImportError("The pymongo package is not available")
         return MongoBackend(config_info["url"])
     else:
-        raise ValueError("Unknown backend %s for TAXII server ".format(config_info["backend"]))
+        raise ValueError("Unknown backend {} for TAXII server".format(config_info["backend"]))
 
 
-_BACKEND = None
-
-
-def init_backend(config_info):
-    global _BACKEND
-    if _BACKEND is None:
-        _BACKEND = connect_to_backend(config_info)
-
-
-def get_backend():
-    return _BACKEND
+def init_backend(flask_application_instance, config_info):
+    with flask_application_instance.app_context():
+        log.debug("Registering medallion_backend into {}".format(current_app))
+        current_app.medallion_backend = connect_to_backend(config_info)
 
 
 @auth.get_password
@@ -70,13 +63,14 @@ def get_pwd(username):
 
 
 def register_blueprints(flask_application_instance):
-    log.debug("Registering medallion blueprints")
     from medallion.views import collections
     from medallion.views import discovery
     from medallion.views import manifest
     from medallion.views import objects
 
-    flask_application_instance.register_blueprint(collections.mod)
-    flask_application_instance.register_blueprint(discovery.mod)
-    flask_application_instance.register_blueprint(manifest.mod)
-    flask_application_instance.register_blueprint(objects.mod)
+    with flask_application_instance.app_context():
+        log.debug("Registering medallion blueprints into {}".format(current_app))
+        current_app.register_blueprint(collections.mod)
+        current_app.register_blueprint(discovery.mod)
+        current_app.register_blueprint(manifest.mod)
+        current_app.register_blueprint(objects.mod)
