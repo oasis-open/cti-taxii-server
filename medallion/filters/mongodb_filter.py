d9242f9062f8bdda0ca0e3ba444cf660ffe90862
James Fitzsimons <james.fitzsimons@mbie.govt.nz>
2018-07-12 15:54:31 +1200

    Added logic to handle marking-definitions

--- medallion/filters/mongodb_filter.py
+++ medallion/filters/mongodb_filter.py
@@ -44,6 +44,16 @@ class MongoDBFilter(BasicFilter):
             date_filter = {'$match': {'date_added': {'$gt': added_after_timestamp}}}
             pipeline.append(date_filter)
 
+        # need to handle marking-definitions differently as they are not versioned like SDO's
+        if self.filter_contains_marking_definition(pipeline):
+            # If we are finding marking-definitions from the objects collection we need to change the match criteria from "_type" to "type"
+            if data.name == "objects" and "_type" in pipeline[0]["$match"].keys():
+                pipeline[0]["$match"]["type"] = pipeline[0]["$match"].pop("_type")
+            cursor = data.aggregate(pipeline)
+            results = list(cursor)   
+
+            return results 
+
         # create version filter
         if "version" in allowed:
             match_version = self.filter_args.get("match[version]")
@@ -81,7 +91,7 @@ class MongoDBFilter(BasicFilter):
                     }
                 }
                 pipeline.append(version_filter)
-
+   
         if data._Collection__name == "manifests":
             cursor = data.aggregate(pipeline)
             results = list(cursor)
@@ -116,7 +126,7 @@ class MongoDBFilter(BasicFilter):
             pipeline.append(project_objects)
             # denormalise the embedded objects and replace the document root
             pipeline.append({'$unwind': '$obj'})
-            pipeline.append({'$replaceRoot': {'newRoot': "$obj"}})
+            pipeline.append({'$replaceRoot': {'newRoot': "$obj"}})     
             # Redact the result set removing objects where the modified date is not in
             # the versions array
             redact_objects = {
@@ -144,3 +154,19 @@ class MongoDBFilter(BasicFilter):
             results = list(cursor)
 
         return results
+
+    def filter_contains_marking_definition(self, pipeline):
+        # If we are matching on id (either match[id]= or /{id}), then check if we are trying to find a marking definition.
+        # If so, we don't want do filter by version as marking-definition objects are not versioned. 
+        if ("id" in pipeline[0]["$match"].keys() and pipeline[0]["$match"]["id"].startswith("marking-definition")):
+            return True
+
+        if "_type" in pipeline[0]["$match"].keys():
+            if (isinstance(pipeline[0]["$match"]["_type"], dict) and \
+                "$in" in pipeline[0]["$match"]["_type"].keys()) and \
+                ("marking-definition" in pipeline[0]["$match"]["_type"]["$in"]):
+                return True
+            elif (pipeline[0]["$match"]["_type"].startswith("marking-definition")):
+                return True
+
+        return False
