185e7867764e7a3e6e675e02091428001153c867
Emmanuelle Vargas-Gonzalez <emmanuelle@mitre.org>
2019-06-28 15:02:37 -0400

    consistency and apply changes made to permission methods

--- medallion/views/discovery.py
+++ medallion/views/discovery.py
@@ -1,10 +1,10 @@
-import flask
-from flask import Blueprint, Response, abort, current_app
+from flask import Blueprint, Response, current_app, json
 
 from medallion import auth
+from medallion.exceptions import ProcessingError
 from medallion.views import MEDIA_TYPE_TAXII_V20
 
-mod = Blueprint('discovery', __name__)
+mod = Blueprint("discovery", __name__)
 
 
 @mod.route("/taxii/", methods=["GET"])
@@ -15,7 +15,13 @@ def get_server_discovery():
     # depending upon the credentials.
     server_discovery = current_app.medallion_backend.server_discovery()
 
-    return Response(response=flask.json.dumps(server_discovery), status=200, mimetype=MEDIA_TYPE_TAXII_V20)
+    if server_discovery:
+        return Response(
+            response=json.dumps(server_discovery),
+            status=200,
+            mimetype=MEDIA_TYPE_TAXII_V20,
+        )
+    raise ProcessingError("Server discovery information not available", 404)
 
 
 @mod.route("/<string:api_root>/", methods=["GET"])
@@ -25,8 +31,12 @@ def get_api_root_information(api_root):
     root_info = current_app.medallion_backend.get_api_root_information(api_root)
 
     if root_info:
-        return Response(response=flask.json.dumps(root_info), status=200, mimetype=MEDIA_TYPE_TAXII_V20)
-    abort(404)
+        return Response(
+            response=json.dumps(root_info),
+            status=200,
+            mimetype=MEDIA_TYPE_TAXII_V20,
+        )
+    raise ProcessingError("API root '{}' information not found".format(api_root), 404)
 
 
 @mod.route("/<string:api_root>/status/<string:id_>/", methods=["GET"])
@@ -36,5 +46,9 @@ def get_status(api_root, id_):
     status = current_app.medallion_backend.get_status(api_root, id_)
 
     if status:
-        return Response(response=flask.json.dumps(status), status=200, mimetype=MEDIA_TYPE_TAXII_V20)
-    abort(404)
+        return Response(
+            response=json.dumps(status),
+            status=200,
+            mimetype=MEDIA_TYPE_TAXII_V20,
+        )
+    raise ProcessingError("Status '{}' not found".format(id_), 404)
--- medallion/views/manifest.py
+++ medallion/views/manifest.py
@@ -1,7 +1,7 @@
-import flask
-from flask import Blueprint, Response, abort, current_app, request
+from flask import Blueprint, Response, current_app, json, request
 
 from medallion import auth
+from medallion.exceptions import ProcessingError
 from medallion.views import MEDIA_TYPE_TAXII_V20
 from medallion.views.objects import (collection_exists,
                                      get_range_request_from_headers,
@@ -15,20 +15,17 @@ mod = Blueprint("manifest", __name__)
 @auth.login_required
 def get_object_manifest(api_root, id_):
 
-    if not collection_exists(api_root, id_):
-        abort(404)
-
-    if not permission_to_read(api_root, id_):
-        abort(403)
-
-    start_index, end_index = get_range_request_from_headers(request)
-    total_count, manifest = current_app.medallion_backend.get_object_manifest(api_root, id_, request.args, ("id", "type", "version"),
-                                                                              start_index, end_index)
-
-    status, headers = get_response_status_and_headers(start_index, total_count, manifest)
-    if manifest:
-        return Response(response=flask.json.dumps({"objects": manifest}),
-                        status=status,
-                        mimetype=MEDIA_TYPE_TAXII_V20,
-                        headers=headers)
-    abort(404)
+    if collection_exists(api_root, id_) and permission_to_read(api_root, id_):
+        start_index, end_index = get_range_request_from_headers()
+        total_count, manifest = current_app.medallion_backend.get_object_manifest(
+            api_root, id_, request.args, ("id", "type", "version"), start_index, end_index
+        )
+        status, headers = get_response_status_and_headers(start_index, total_count, manifest)
+        if manifest:
+            return Response(
+                response=json.dumps({"objects": manifest}),
+                status=status,
+                headers=headers,
+                mimetype=MEDIA_TYPE_TAXII_V20,
+            )
+        raise ProcessingError("Collection '{}' has no manifests available".format(id_), 404)
