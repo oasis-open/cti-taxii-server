dc56061f7d44f0c97f86e43a2a7cbfe0134508db
Emmanuelle Vargas-Gonzalez <emmanuelle@mitre.org>
2019-10-11 12:57:14 -0400

    add documentation for all the views

--- medallion/views/collections.py
+++ medallion/views/collections.py
@@ -10,6 +10,17 @@ mod = Blueprint("collections", __name__)
 @mod.route("/<string:api_root>/collections/", methods=["GET"])
 @auth.login_required
 def get_collections(api_root):
+    """
+    Defines TAXII API - Collections:
+    Get Collection section (5.2) <link here>`__
+
+    Args:
+        api_root (str): the base URL of the API Root
+
+    Returns:
+        collections: A Collections Resource upon successful requests. Additional information here <link here>`__.
+
+    """
     # TODO: Check if user has access to the each collection's metadata - unrelated to can_read, can_write attributes
     collections = current_app.medallion_backend.get_collections(api_root)
     if collections:
@@ -24,6 +35,18 @@ def get_collections(api_root):
 @mod.route("/<string:api_root>/collections/<string:collection_id>/", methods=["GET"])
 @auth.login_required
 def get_collection(api_root, collection_id):
+    """
+    Defines TAXII API - Collections:
+    Get Collection section (5.2) <link here>`__
+
+    Args:
+        api_root (str): the base URL of the API Root
+        collection_id (str): the `identifier` of the Collection being requested
+
+    Returns:
+        collection: A Collection Resource upon successful requests. Additional information here <link here>`__.
+
+    """
     # TODO: Check if user has access to the collection's metadata - unrelated to can_read, can_write attributes
     collection = current_app.medallion_backend.get_collection(api_root, collection_id)
     if collection:
--- medallion/views/manifest.py
+++ medallion/views/manifest.py
@@ -11,7 +11,18 @@ mod = Blueprint("manifest", __name__)
 @mod.route("/<string:api_root>/collections/<string:collection_id>/manifest/", methods=["GET"])
 @auth.login_required
 def get_object_manifest(api_root, collection_id):
+    """
+    Defines TAXII API - Collections:
+    Get Object Manifests section (5.3) <link here>`__
 
+    Args:
+        api_root (str): the base URL of the API Root
+        collection_id (str): the `identifier` of the Collection being requested
+
+    Returns:
+        manifest: A Manifest Resource upon successful requests. Additional information here <link here>`__.
+
+    """
     if collection_exists(api_root, collection_id) and permission_to_read(api_root, collection_id):
         manifests = current_app.medallion_backend.get_object_manifest(
             api_root, collection_id, request.args, ("id", "type", "version", "spec_version"),
