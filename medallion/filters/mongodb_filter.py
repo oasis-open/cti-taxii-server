a3803c64ad3db7b434a29a54a799cd5024ebbcaf
Emmanuelle Vargas-Gonzalez <emmanuelle@mitre.org>
2019-10-11 13:41:45 -0400

    update MongoDB process_filter per changes to spec:
    - New manifest structure meant the pipeline required updates
    - Better support for single version objects (markings) and unversioned (SCOs)
    - Added some logic for limit Pagination but still not in use

--- medallion/filters/mongodb_filter.py
+++ medallion/filters/mongodb_filter.py
@@ -39,35 +39,25 @@ class MongoDBFilter(BasicFilter):
         return parameters
 
     def process_filter(self, data, allowed, manifest_info):
-        match_filter = {"$match": self.full_query}
+        match_filter = {
+            "$match": {
+                "$and": [self.full_query],
+                "$comment": "Step #1: Match against an object/manifest id or type present in a collection.",
+            },
+        }
         pipeline = [match_filter]
 
         # create added_after filter
         added_after_date = self.filter_args.get("added_after")
         if added_after_date:
             added_after_timestamp = convert_to_stix_datetime(added_after_date)
-            date_filter = {"$match": {"date_added": {"$gt": added_after_timestamp}}}
+            date_filter = {
+                "$match": {
+                    "date_added": {"$gt": added_after_timestamp},
+                    "$comment": "Step #2: If added_after is provided, remove all objects/manifests older than the provided time",
+                }
+            }
             pipeline.append(date_filter)
-
-        # need to handle marking-definitions differently as they are not versioned like SDO's
-        if self.filter_contains_marking_definition(pipeline):
-            # If we are finding marking-definitions from the objects collection
-            # we need to change the match criteria from "_type" to "type"
-            if data.name == "objects" and "_type" in pipeline[0]["$match"].keys():
-                pipeline[0]["$match"]["type"] = pipeline[0]["$match"].pop("_type")
-
-            # Calculate total number of matching documents
-            if data.name == "objects":
-                count = self.get_result_count(pipeline, manifest_info["mongodb_collection"])
-            else:
-                count = self.get_result_count(pipeline, data)
-
-            self.add_pagination_operations(pipeline)
-
-            cursor = data.aggregate(pipeline)
-            results = list(cursor)
-
-            return count, results
 
         # create version filter
         if "version" in allowed:
@@ -77,14 +67,12 @@ class MongoDBFilter(BasicFilter):
             if "all" not in match_version:
                 actual_dates = [x for x in match_version.split(",") if (x != "first" and x != "last")]
                 # If specific dates have been selected, then we add these to the $match criteria
-                # created from the self.full_query at the beginning of this method. The reason we need
-                # to do this is because the $indexOfArray function below will return -1 if the date
-                # doesn't exist in the versions array. -1 will be interpreted by $arrayElemAt as the
-                # final element in the array and we will return the wrong result. i.e. not only will the
-                # version dates be incorrect, but we shouldn't have returned a result at all.
-                # if actual_dates:
+                # created from the self.full_query at the beginning of this method. This will make
+                # sure we can pick the correct manifests even if `added_after` later modifies this results.
                 if len(actual_dates) > 0:
-                    pipeline.insert(1, {"$match": {"versions": {"$all": [",".join(actual_dates)]}}})
+                    pipeline[0]["$match"]["$and"].append({"version": {"$in": actual_dates}})
+
+                pipeline.append({"$group": {"_id": "$id", "versions": {"$push": "$$ROOT"}}})
 
                 # The versions array in the mongodb document is ordered newest to oldest, so the 'last'
                 # (most recent date) is in first position in the list and the oldest 'first' is in
@@ -96,118 +84,88 @@ class MongoDBFilter(BasicFilter):
                     version_selector.append({"$arrayElemAt": ["$versions", -1]})
                 for d in actual_dates:
                     version_selector.append({"$arrayElemAt": ["$versions", {"$indexOfArray": ["$versions", d]}]})
-                version_filter = {
-                    "$addFields": {
-                        "versions": version_selector,
-                    },
-                }
-                pipeline.append(version_filter)
+                pipeline.append({"$addFields": {"versions": version_selector}})
+
+                # denormalize the embedded objects, remove duplicates and replace the document root. Could be improved
+                pipeline.append({"$unwind": "$versions"})
+                pipeline.append({"$replaceRoot": {"newRoot": "$versions"}})
+                pipeline.append({"$group": {"_id": "$$ROOT"}})
+                pipeline.append({"$replaceRoot": {"newRoot": "$_id"}})
+        pipeline.append({"$sort": {"date_added": pymongo.ASCENDING}})
+        # self.add_pagination_operations(pipeline, self.filter_args.get("limit"))
 
         if data.name == "manifests":
-            count = self.get_result_count(pipeline, data)
-            self.add_pagination_operations(pipeline)
-
             cursor = data.aggregate(pipeline)
             results = list(cursor)
         else:
-            results = []
-            # Get the count of matching documents - need to unwind the versions selected to get accurate count.
-            count_pipeline = list(pipeline)
-            count_pipeline.append({"$unwind": "$versions"})
-            count = self.get_result_count(count_pipeline, manifest_info["mongodb_collection"])
+            # Join the filtered manifest(s) to the objects collection
+            join_objects = {
+                "$lookup": {
+                    "from": "objects",
+                    "localField": "id",
+                    "foreignField": "id",
+                    "as": "obj",
+                },
+            }
+            pipeline.append(join_objects)
 
-            # only bother doing the rest of the query if the start index is less than the total number of results.
-            if self.start_index < count:
-                # Join the filtered manifest(s) to the objects collection
-                join_objects = {
-                    "$lookup": {
-                        "from": "objects",
-                        "localField": "id",
-                        "foreignField": "id",
-                        "as": "obj",
-                    },
-                }
-                pipeline.append(join_objects)
-                # Copy the filtered version list to the embedded object document
-                add_versions = {
-                    "$addFields": {"obj.versions": "$versions"},
-                }
-                pipeline.append(add_versions)
-                # denormalize the embedded objects and replace the document root
-                pipeline.append({"$unwind": "$obj"})
-                pipeline.append({"$replaceRoot": {"newRoot": "$obj"}})
-                # Redact the result set removing objects where the modified date is not in
-                # the versions array and the object isn't in the correct collection.
-                # The collection filter is required because the join between manifests and objects
-                # does not include collection_id
-                col_id = self.full_query["_collection_id"]
-                redact_objects = {
-                    "$redact": {
-                        "$cond": {
-                            "if": {
-                                "$and": [
-                                    {"$eq": ["$_collection_id", col_id]},
-                                    {
-                                        "$or": [
-                                            {"$eq": ["$type", "marking-definition"]},
-                                            {"$setIsSubset": [["$modified"], "$versions"]},
+            # Copy the filtered version list to the embedded object document
+            add_versions = {
+                "$addFields": {"obj.version": "$version"},
+            }
+            pipeline.append(add_versions)
+
+            # denormalize the embedded objects and replace the document root
+            pipeline.append({"$unwind": "$obj"})
+            pipeline.append({"$replaceRoot": {"newRoot": "$obj"}})
+
+            # Redact the result set removing objects where the modified date is not in
+            # the versions array and the object isn't in the correct collection.
+            # The collection filter is required because the join between manifests and objects
+            # does not include collection_id
+            col_id = self.full_query["_collection_id"]["$eq"]
+            redact_objects = {
+                "$redact": {
+                    "$cond": {
+                        "if": {
+                            "$and": [
+                                {"$eq": ["$_collection_id", col_id]},
+                                {
+                                    "$switch": {
+                                        "branches": [
+                                            {"case": {"$eq": ["$modified", "$version"]}, "then": True},
+                                            {"case": {"$eq": ["$created", "$version"]}, "then": True},
+                                            {"case": {"$eq": ["$_date_added", "$version"]}, "then": True},
                                         ],
+                                        "default": False,
                                     },
-                                ],
-                            },
-                            "then": "$$KEEP",
-                            "else": "$$PRUNE",
+                                },
+                            ],
                         },
+                        "then": "$$KEEP",
+                        "else": "$$PRUNE",
                     },
-                }
-                pipeline.append(redact_objects)
-                # Project the final results
-                project_results = {
-                    "$project": {
-                        "versions": 0,
-                    },
-                }
-                pipeline.append(project_results)
-                self.add_pagination_operations(pipeline)
+                },
+            }
+            pipeline.append(redact_objects)
 
-                cursor = manifest_info["mongodb_collection"].aggregate(pipeline)
-                results = list(cursor)
+            # Project the final results
+            project_results = {"$project": {"version": 0}}
+            pipeline.append(project_results)
 
-        return count, results
+            # denormalize the embedded objects, remove duplicates and replace the document root. Could be improved
+            pipeline.append({"$group": {"_id": "$$ROOT"}})
+            pipeline.append({"$replaceRoot": {"newRoot": "$_id"}})
+            pipeline.append({"$sort": {"_date_added": pymongo.ASCENDING, "created": pymongo.ASCENDING, "modified": pymongo.ASCENDING}})
+            # self.add_pagination_operations(pipeline, self.filter_args.get("limit"))
 
-    def add_pagination_operations(self, pipeline):
-        if self.start_index is not None and self.end_index is not None:
-            pipeline.append({"$skip": self.start_index})
-            pipeline.append({"$limit": (self.end_index - self.start_index) + 1})
+            cursor = manifest_info["mongodb_manifests_collection"].aggregate(pipeline)
+            results = list(cursor)
 
-    def get_result_count(self, pipeline, data):
-        count_pipeline = list(pipeline)
-        count_pipeline.append({"$count": "total_count"})
-        count_result = list(data.aggregate(count_pipeline))
+        return results
 
-        if len(count_result) == 0:
-            # No results
-            return 0
-
-        count = count_result[0]["total_count"]
-
-        return count
-
-    def filter_contains_marking_definition(self, pipeline):
-        # If we are matching on id (either match[id]= or /{id}), then check if
-        # we are trying to find a marking definition. If so, we don't want do
-        # filter by version as marking-definition objects are not versioned.
-        if "id" in pipeline[0]["$match"].keys() and pipeline[0]["$match"]["id"].startswith("marking-definition"):
-            return True
-
-        if "_type" in pipeline[0]["$match"].keys():
-            if ((
-                isinstance(pipeline[0]["$match"]["_type"], dict) and
-                "$in" in pipeline[0]["$match"]["_type"].keys()
-            ) and
-                    ("marking-definition" in pipeline[0]["$match"]["_type"]["$in"])):
-                return True
-            elif pipeline[0]["$match"]["_type"].startswith("marking-definition"):
-                return True
-
-        return False
+    def add_pagination_operations(self, pipeline, limit=0):
+        limit = int(limit)
+        if limit > 0:
+            pipeline.append({"$skip": limit})
+            pipeline.append({"$limit": limit})
