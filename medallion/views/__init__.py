53fffff0bc445364b5a59d1a10c48511af21e4e9
Emmanuelle Vargas-Gonzalez <emmanuelle@mitre.org>
2019-10-18 14:52:20 -0400

    update media_type to new format

--- medallion/views/__init__.py
+++ medallion/views/__init__.py
@@ -1,2 +1,2 @@
-MEDIA_TYPE_TAXII_ANY = "application/vnd.oasis.taxii+json"
-MEDIA_TYPE_TAXII_V21 = "{media}; version=2.1".format(media=MEDIA_TYPE_TAXII_ANY)
+MEDIA_TYPE_TAXII_ANY = "application/taxii+json"
+MEDIA_TYPE_TAXII_V21 = "{media};version=2.1".format(media=MEDIA_TYPE_TAXII_ANY)
