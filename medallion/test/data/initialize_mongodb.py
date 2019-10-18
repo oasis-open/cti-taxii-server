e5eff0dcd28ed756bf870ef6fd80dade890f4f29
Emmanuelle Vargas-Gonzalez <emmanuelle@mitre.org>
2019-10-18 14:47:05 -0400

    update default data across both backends:
    - Update objects to reflect true STIX objects (proper uuid, missing properties)
    - Moved all objects to a single collection
    - Updated manifests to be per object version. Fixed precision on TAXII resources to be microsecond
    - Align data between Memory and Mongo backend.
    - Manifests are sorted by date_added, Objects by modified, then created ASCENDING

--- medallion/test/data/initialize_mongodb.py
+++ medallion/test/data/initialize_mongodb.py
@@ -16,6 +16,25 @@ def reset_db(url="mongodb://root:example@localhost:27017/"):
         "contact": "string containing contact information",
         "api_roots": [],
     })
+
+    client.drop_database("api1")
+    add_api_root(
+        client,
+        url="http://localhost:5000/api1/",
+        title="General STIX 2.1 Collections",
+        description="A repo for general STIX data.",
+        max_content_length=9765625,
+    )
+
+    client.drop_database("api2")
+    add_api_root(
+        client,
+        url="http://localhost:5000/api2/",
+        title="STIX 2.1 Indicator Collections",
+        description="A repo for general STIX data.",
+        max_content_length=9765625,
+    )
+
     client.drop_database("trustgroup1")
     api_root_db = add_api_root(
         client,
@@ -29,13 +48,14 @@ def reset_db(url="mongodb://root:example@localhost:27017/"):
         {
             "id": "2d086da7-4bdc-4f91-900e-d77486753710",
             "status": "pending",
-            "request_timestamp": "2016-11-02T12:34:34.12345Z",
+            "request_timestamp": "2016-11-02T12:34:34.123456Z",
             "total_count": 4,
             "success_count": 1,
             "successes": [
                 {
                     "id": "indicator--a932fcc6-e032-176c-126f-cb970a5a1ade",
                     "version": "2014-05-08T09:00:00.000Z",
+                    "message": "Successfully added object to collection '91a7b528-80eb-42ed-a74d-c6fbd5a26116'."
                 }
             ],
             "failure_count": 1,
@@ -61,8 +81,8 @@ def reset_db(url="mongodb://root:example@localhost:27017/"):
         {
             "id": "2d086da7-4bdc-4f91-900e-f4566be4b780",
             "status": "pending",
-            "request_timestamp": "2016-11-02T12:34:34.12345Z",
-            "total_objects": 2,
+            "request_timestamp": "2016-11-02T12:34:34.123456Z",
+            "total_objects": 0,
             "success_count": 0,
             "successes": [],
             "failure_count": 0,
@@ -74,81 +94,81 @@ def reset_db(url="mongodb://root:example@localhost:27017/"):
 
     api_root_db["manifests"].insert_many([
         {
-            "id": "indicator--a932fcc6-e032-176c-126f-cb970a5a1ade",
-            "date_added": convert_to_stix_datetime("2016-11-01T03:04:05.000Z"),
-            "version": "2014-05-08T09:00:00.000Z",
-            "media_type": "application/stix+json; version=2.1",
-            "_collection_id": "91a7b528-80eb-42ed-a74d-c6fbd5a26116",
-            "_type": "indicator",
-        },
-        {
-            "id": "malware--fdd60b30-b67c-11e3-b0b9-f01faf20d111",
-            "date_added": convert_to_stix_datetime("2017-01-27T13:49:53.997Z"),
-            "version": "2017-01-27T13:49:53.997Z",
-            "media_type": "application/stix+json; version=2.1",
-            "_collection_id": "91a7b528-80eb-42ed-a74d-c6fbd5a26116",
-            "_type": "malware",
-        },
-        {
+            "date_added": convert_to_stix_datetime("2014-05-08T09:00:00.000000Z"),
             "id": "relationship--2f9a9aa9-108a-4333-83e2-4fb25add0463",
-            "date_added": convert_to_stix_datetime("2014-05-08T09:00:00.000Z"),
+            "media_type": "application/stix+json;version=2.1",
             "version": "2014-05-08T09:00:00.000Z",
-            "media_type": "application/stix+json; version=2.1",
             "_collection_id": "91a7b528-80eb-42ed-a74d-c6fbd5a26116",
             "_type": "relationship",
         },
         {
+            "date_added": convert_to_stix_datetime("2016-11-01T03:04:05.000000Z"),
+            "id": "indicator--cd981c25-8042-4166-8945-51178443bdac",
+            "media_type": "application/stix+json;version=2.1",
+            "version": "2014-05-08T09:00:00.000Z",
+            "_collection_id": "91a7b528-80eb-42ed-a74d-c6fbd5a26116",
+            "_type": "indicator",
+        },
+        {
+            "date_added": convert_to_stix_datetime("2016-11-03T12:30:59.001000Z"),
+            "id": "indicator--6770298f-0fd8-471a-ab8c-1c658a46574e",
+            "media_type": "application/stix+json;version=2.1",
+            "version": "2016-11-03T12:30:59.000Z",
+            "_collection_id": "91a7b528-80eb-42ed-a74d-c6fbd5a26116",
+            "_type": "indicator",
+        },
+        {
+            "date_added": convert_to_stix_datetime("2016-12-27T13:49:59.000000Z"),
+            "id": "indicator--6770298f-0fd8-471a-ab8c-1c658a46574e",
+            "media_type": "application/stix+json;version=2.1",
+            "version": "2016-12-25T12:30:59.444Z",
+            "_collection_id": "91a7b528-80eb-42ed-a74d-c6fbd5a26116",
+            "_type": "indicator",
+        },
+        {
+            "date_added": convert_to_stix_datetime("2017-01-20T00:00:00.000000Z"),
             "id": "marking-definition--34098fce-860f-48ae-8e50-ebd3cc5e41da",
-            "date_added": convert_to_stix_datetime("2017-01-20T00:00:00.000Z"),
+            "media_type": "application/stix+json;version=2.1",
             "version": "2017-01-20T00:00:00.000Z",
-            "media_type": "application/stix+json; version=2.1",
             "_collection_id": "91a7b528-80eb-42ed-a74d-c6fbd5a26116",
             "_type": "marking-definition",
         },
         {
-            "id": "indicator--b81f86b9-975b-bb0b-775e-810c5bd45b4f",
-            "date_added": convert_to_stix_datetime("2017-12-31T13:49:53.935Z"),
+            "date_added": convert_to_stix_datetime("2017-01-27T13:49:59.997000Z"),
+            "id": "malware--c0931cc6-c75e-47e5-9036-78fabc95d4ec",
+            "media_type": "application/stix+json;version=2.1",
+            "version": "2017-01-27T13:49:53.997Z",
+            "_collection_id": "91a7b528-80eb-42ed-a74d-c6fbd5a26116",
+            "_type": "malware",
+        },
+        {
+            "date_added": convert_to_stix_datetime("2017-12-31T13:49:53.935000Z"),
+            "id": "indicator--6770298f-0fd8-471a-ab8c-1c658a46574e",
+            "media_type": "application/stix+json;version=2.1",
             "version": "2017-01-27T13:49:53.935Z",
-            "media_type": "application/stix+json; version=2.1",
             "_collection_id": "91a7b528-80eb-42ed-a74d-c6fbd5a26116",
             "_type": "indicator",
-        },
-        {
-            "id": "indicator--b81f86b9-975b-bb0b-775e-810c5bd45b4f",
-            "date_added": convert_to_stix_datetime("2016-12-27T13:49:59.000Z"),
-            "version": "2016-12-25T12:30:59.444Z",
-            "media_type": "application/stix+json; version=2.1",
-            "_collection_id": "91a7b528-80eb-42ed-a74d-c6fbd5a26116",
-            "_type": "indicator",
-        },
-        {
-            "id": "indicator--b81f86b9-975b-bb0b-775e-810c5bd45b4f",
-            "date_added": convert_to_stix_datetime("2016-11-03T12:30:59.001Z"),
-            "version": "2016-11-03T12:30:59.000Z",
-            "media_type": "application/stix+json; version=2.1",
-            "_collection_id": "91a7b528-80eb-42ed-a74d-c6fbd5a26116",
-            "_type": "indicator",
-        },
+        }
     ])
 
     api_root_db["collections"].insert_one({
-        "id": "91a7b528-80eb-42ed-a74d-c6fbd5a26116",
-        "title": "High Value Indicator Collection",
-        "can_read": True,
+        "id": "472c94ae-3113-4e3e-a4dd-a9f4ac7471d4",
+        "title": "This data collection is for testing querying across collections",
+        "can_read": False,
         "can_write": True,
         "media_types": [
-            "application/stix+json; version=2.1",
+            "application/stix+json;version=2.1",
         ],
     })
 
     api_root_db["collections"].insert_one({
-        "id": "472c94ae-3113-4e3e-a4dd-a9f4ac7471d4",
-        "title": "Empty test Collection",
-        "description": "This data collection is for testing querying across collections",
+        "id": "91a7b528-80eb-42ed-a74d-c6fbd5a26116",
+        "title": "High Value Indicator Collection",
+        "description": "This data collection is for collecting high value IOCs",
         "can_read": True,
         "can_write": True,
         "media_types": [
-            "application/stix+json; version=2.1",
+            "application/stix+json;version=2.1",
         ],
     })
 
@@ -159,7 +179,7 @@ def reset_db(url="mongodb://root:example@localhost:27017/"):
         "can_read": True,
         "can_write": False,
         "media_types": [
-            "application/stix+json; version=2.1",
+            "application/stix+json;version=2.1",
         ],
     })
 
@@ -170,115 +190,112 @@ def reset_db(url="mongodb://root:example@localhost:27017/"):
         "can_read": False,
         "can_write": False,
         "media_types": [
-            "application/stix+json; version=2.1",
+            "application/stix+json;version=2.1",
         ],
     })
 
     api_root_db["objects"].insert_many([
         {
-            "created": "2016-11-03T12:30:59.000Z",
-            "id": "indicator--b81f86b9-975b-bb0b-775e-810c5bd45b4f",
+            "created": "2014-05-08T09:00:00.000Z",
+            "modified": "2014-05-08T09:00:00.000Z",
+            "id": "relationship--2f9a9aa9-108a-4333-83e2-4fb25add0463",
+            "relationship_type": "indicates",
+            "source_ref": "indicator--cd981c25-8042-4166-8945-51178443bdac",
             "spec_version": "2.1",
-            "indicator_types": [
-                "url-watchlist",
-            ],
-            "modified": "2017-01-27T13:49:53.935Z",
-            "name": "Malicious site hosting downloader",
-            "description": "Accessing this url will infect your machine with malware. This is the last updated indicator",
-            "pattern": "[url:value = 'http://x4z9arb.cn/4712']",
-            "type": "indicator",
-            "valid_from": "2016-11-03T12:30:59.000Z",
-            "_collection_id": "91a7b528-80eb-42ed-a74d-c6fbd5a26116",
-        },
-        {
-            "created": "2016-11-03T12:30:59.000Z",
-            "description": "Accessing this url will infect your machine with malware. Updated indicator",
-            "spec_version": "2.1",
-            "id": "indicator--b81f86b9-975b-bb0b-775e-810c5bd45b4f",
-            "indicator_types": [
-                "url-watchlist",
-            ],
-            "modified": "2016-12-25T12:30:59.444Z",
-            "name": "Malicious site hosting downloader",
-            "pattern": "[url:value = 'http://x4z9arb.cn/4712']",
-            "type": "indicator",
-            "valid_from": "2017-01-27T13:49:53.935382Z",
-            "_collection_id": "91a7b528-80eb-42ed-a74d-c6fbd5a26116",
-        },
-        {
-            "created": "2017-01-27T13:49:53.997Z",
-            "description": "Poison Ivy",
-            "id": "malware--fdd60b30-b67c-11e3-b0b9-f01faf20d111",
-            "spec_version": "2.1",
-            "malware_types": [
-                "remote-access-trojan",
-            ],
-            "modified": "2017-01-27T13:49:53.997Z",
-            "name": "Poison Ivy",
-            "type": "malware",
+            "target_ref": "malware--c0931cc6-c75e-47e5-9036-78fabc95d4ec",
+            "type": "relationship",
             "_collection_id": "91a7b528-80eb-42ed-a74d-c6fbd5a26116",
         },
         {
             "created": "2014-05-08T09:00:00.000Z",
-            "spec_version": "2.1",
-            "id": "indicator--a932fcc6-e032-176c-126f-cb970a5a1ade",
+            "id": "indicator--cd981c25-8042-4166-8945-51178443bdac",
             "indicator_types": [
                 "file-hash-watchlist",
             ],
             "modified": "2014-05-08T09:00:00.000Z",
             "name": "File hash for Poison Ivy variant",
             "pattern": "[file:hashes.'SHA-256' = 'ef537f25c895bfa782526529a9b63d97aa631564d5d789c2b765448c8635fb6c']",
+            "pattern_type": "stix",
+            "spec_version": "2.1",
             "type": "indicator",
             "valid_from": "2014-05-08T09:00:00.000000Z",
             "_collection_id": "91a7b528-80eb-42ed-a74d-c6fbd5a26116",
         },
         {
-            "created": "2014-05-08T09:00:00.000Z",
-            "spec_version": "2.1",
-            "id": "relationship--2f9a9aa9-108a-4333-83e2-4fb25add0463",
-            "modified": "2014-05-08T09:00:00.000Z",
-            "relationship_type": "indicates",
-            "source_ref": "indicator--a932fcc6-e032-176c-126f-cb970a5a1ade",
-            "target_ref": "malware--fdd60b30-b67c-11e3-b0b9-f01faf20d111",
-            "type": "relationship",
-            "_collection_id": "91a7b528-80eb-42ed-a74d-c6fbd5a26116",
-        },
-        {
-            "type": "marking-definition",
-            "spec_version": "2.1",
-            "id": "marking-definition--34098fce-860f-48ae-8e50-ebd3cc5e41da",
-            "created": "2017-01-20T00:00:00.000Z",
-            "definition_type": "tlp",
-            "definition": {
-                "tlp": "green",
-            },
-            "_collection_id": "91a7b528-80eb-42ed-a74d-c6fbd5a26116",
-        },
-        {
             "created": "2016-11-03T12:30:59.000Z",
-            "spec_version": "2.1",
             "description": "Accessing this url will infect your machine with malware.",
-            "id": "indicator--b81f86b9-975b-bb0b-775e-810c5bd45b4f",
+            "id": "indicator--6770298f-0fd8-471a-ab8c-1c658a46574e",
             "indicator_types": [
                 "url-watchlist",
             ],
             "modified": "2016-11-03T12:30:59.000Z",
             "name": "Malicious site hosting downloader",
             "pattern": "[url:value = 'http://z4z10farb.cn/4712']",
+            "pattern_type": "stix",
+            "spec_version": "2.1",
             "type": "indicator",
             "valid_from": "2017-01-27T13:49:53.935382Z",
             "_collection_id": "91a7b528-80eb-42ed-a74d-c6fbd5a26116",
         },
+        {
+            "created": "2016-11-03T12:30:59.000Z",
+            "description": "Accessing this url will infect your machine with malware. Updated indicator",
+            "id": "indicator--6770298f-0fd8-471a-ab8c-1c658a46574e",
+            "indicator_types": [
+                "url-watchlist",
+            ],
+            "modified": "2016-12-25T12:30:59.444Z",
+            "name": "Malicious site hosting downloader",
+            "pattern": "[url:value = 'http://x4z9arb.cn/4712']",
+            "pattern_type": "stix",
+            "spec_version": "2.1",
+            "type": "indicator",
+            "valid_from": "2017-01-27T13:49:53.935382Z",
+            "_collection_id": "91a7b528-80eb-42ed-a74d-c6fbd5a26116",
+        },
+        {
+            "created": "2016-11-03T12:30:59.000Z",
+            "description": "Accessing this url will infect your machine with malware. This is the last updated indicator",
+            "id": "indicator--6770298f-0fd8-471a-ab8c-1c658a46574e",
+            "indicator_types": [
+                "url-watchlist",
+            ],
+            "modified": "2017-01-27T13:49:53.935Z",
+            "name": "Malicious site hosting downloader",
+            "pattern": "[url:value = 'http://x4z9arb.cn/4712']",
+            "pattern_type": "stix",
+            "spec_version": "2.1",
+            "type": "indicator",
+            "valid_from": "2016-11-03T12:30:59.000Z",
+            "_collection_id": "91a7b528-80eb-42ed-a74d-c6fbd5a26116",
+        },
+        {
+            "created": "2017-01-20T00:00:00.000Z",
+            "definition": {
+                "tlp": "green",
+            },
+            "definition_type": "tlp",
+            "id": "marking-definition--34098fce-860f-48ae-8e50-ebd3cc5e41da",
+            "name": "TLP:GREEN",
+            "spec_version": "2.1",
+            "type": "marking-definition",
+            "_collection_id": "91a7b528-80eb-42ed-a74d-c6fbd5a26116",
+        },
+        {
+            "created": "2017-01-27T13:49:53.997Z",
+            "description": "Poison Ivy",
+            "id": "malware--c0931cc6-c75e-47e5-9036-78fabc95d4ec",
+            "is_family": True,
+            "malware_types": [
+                "remote-access-trojan",
+            ],
+            "modified": "2017-01-27T13:49:53.997Z",
+            "name": "Poison Ivy",
+            "spec_version": "2.1",
+            "type": "malware",
+            "_collection_id": "91a7b528-80eb-42ed-a74d-c6fbd5a26116",
+        }
     ])
-
-    client.drop_database("api2")
-    api_root_db = add_api_root(
-        client,
-        url="http://localhost:5000/api2/",
-        title="STIX 2.0 Indicator Collections",
-        description="A repo for general STIX data.",
-        max_content_length=9765625,
-    )
 
     date_index = IndexModel([("date_added", ASCENDING)])
     id_index = IndexModel([("id", ASCENDING)])
