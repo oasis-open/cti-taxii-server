385177dbfd21bf23e54095cdf1822f14ce708b00
Emmanuelle Vargas-Gonzalez <emmanuelle@mitre.org>
2019-11-15 14:14:50 -0500

    change views:
    - remove get_custom_headers() to move them into backend internal calls
    - add method to validate_limit_parameter()
    - calls to get_objects(), get_object(), get_object_versions(), get_object_manifest() now return the resource and headers with X-TAXII-Date-Added

--- medallion/views/manifest.py
+++ medallion/views/manifest.py
@@ -3,7 +3,8 @@ from flask import Blueprint, Response, current_app, json, request
 from . import MEDIA_TYPE_TAXII_V21
 from .. import auth
 from ..exceptions import ProcessingError
-from .objects import collection_exists, get_custom_headers, permission_to_read
+from .objects import (collection_exists, permission_to_read,
+                      validate_limit_parameter)
 
 mod = Blueprint("manifest", __name__)
 
@@ -24,11 +25,11 @@ def get_object_manifest(api_root, collection_id):
 
     """
     if collection_exists(api_root, collection_id) and permission_to_read(api_root, collection_id):
-        manifests = current_app.medallion_backend.get_object_manifest(
-            api_root, collection_id, request.args, ("id", "type", "version", "spec_version"),
+        limit = validate_limit_parameter()
+        manifests, headers = current_app.medallion_backend.get_object_manifest(
+            api_root, collection_id, request.args, ("id", "type", "version", "spec_version"), limit
         )
         if manifests:
-            headers = get_custom_headers(api_root, collection_id)
             return Response(
                 response=json.dumps(manifests),
                 status=200,
--- medallion/views/objects.py
+++ medallion/views/objects.py
@@ -33,31 +33,19 @@ def collection_exists(api_root, collection_id):
     raise ProcessingError("Collection '{}' not found".format(collection_id), 404)
 
 
-def get_custom_headers(api_root, id_):
-    headers = {}
-    try:
-        manifest = current_app.medallion_backend.get_object_manifest(
-            api_root, id_, request.args, ("id",),
-        )
-        if manifest:
-            times = sorted(map(lambda x: x["date_added"], manifest["objects"]))
-            if len(times) > 0:
-                headers["X-TAXII-Date-Added-First"] = times[0]
-                headers["X-TAXII-Date-Added-Last"] = times[-1]
-    except Exception as e:
-        log.exception(e)
-    return headers
-
-
-def process_pagination_parameters():
+def validate_limit_parameter():
     limit = request.args.get("limit")
     if limit:
-        limit = int(limit)
-        if limit <= 0:
+        try:
+            limit = int(limit)
+            if limit <= 0:
+                raise
+            if limit > current_app.taxii_config["max_page_size"]:
+                limit = current_app.taxii_config["max_page_size"]
+        except (ValueError, RuntimeError):
             raise ProcessingError("The server did not understand the request or filter parameters", 400)
-        if limit > current_app.taxii_config["max_page_size"]:
-            limit = current_app.taxii_config["max_page_size"]
-        return limit
+        else:
+            return limit
 
 
 @mod.route("/<string:api_root>/collections/<string:collection_id>/objects/", methods=["GET", "POST"])
@@ -81,11 +69,11 @@ def get_or_add_objects(api_root, collection_id):
     request_time = get_timestamp()  # Can't I get this from the request itself?
     if collection_exists(api_root, collection_id):
         if request.method == "GET" and permission_to_read(api_root, collection_id):
-            objects = current_app.medallion_backend.get_objects(
-                api_root, collection_id, request.args, ("id", "type", "version", "spec_version"),
+            limit = validate_limit_parameter()
+            objects, headers = current_app.medallion_backend.get_objects(
+                api_root, collection_id, request.args, ("id", "type", "version", "spec_version"), limit
             )
             if objects:
-                headers = get_custom_headers(api_root, collection_id)
                 return Response(
                     response=json.dumps(objects),
                     status=200,
@@ -126,11 +114,11 @@ def get_or_delete_object(api_root, collection_id, object_id):
 
     if collection_exists(api_root, collection_id):
         if request.method == "GET" and permission_to_read(api_root, collection_id):
-            objects = current_app.medallion_backend.get_object(
-                api_root, collection_id, object_id, request.args, ("version", "spec_version"),
+            limit = validate_limit_parameter()
+            objects, headers = current_app.medallion_backend.get_object(
+                api_root, collection_id, object_id, request.args, ("version", "spec_version"), limit
             )
             if objects:
-                headers = get_custom_headers(api_root, collection_id)
                 return Response(
                     response=json.dumps(objects),
                     status=200,
@@ -167,10 +155,10 @@ def get_object_versions(api_root, collection_id, object_id):
     # TODO: Check if user has access to read objects in collection - right now just check for permissions on the collection.
 
     if collection_exists(api_root, collection_id) and permission_to_read(api_root, collection_id):
-        versions = current_app.medallion_backend.get_object_versions(
-            api_root, collection_id, object_id, request.args, ("spec_version",),
+        limit = validate_limit_parameter()
+        versions, headers = current_app.medallion_backend.get_object_versions(
+            api_root, collection_id, object_id, request.args, ("spec_version",), limit
         )
-        headers = get_custom_headers(api_root, collection_id)
         return Response(
             response=json.dumps(versions),
             status=200,
