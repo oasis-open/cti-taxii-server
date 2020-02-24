c500d43ad2238f4cc879864fbb11f501516f0abf
Michael Chisholm <chisholm@mitre.org>
2020-02-23 19:46:17 -0500

    Fix style error: unused import

--- medallion/views/manifest.py
+++ medallion/views/manifest.py
@@ -2,7 +2,6 @@ from flask import Blueprint, Response, current_app, json, request
 
 from . import MEDIA_TYPE_TAXII_V21, validate_version_parameter_in_accept_header
 from .. import auth
-from ..exceptions import ProcessingError
 from .discovery import api_root_exists
 from .objects import (collection_exists, permission_to_read,
                       validate_limit_parameter)
