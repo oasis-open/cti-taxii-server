19680f42b45b7833339daec711673488142ee131
maybe-sybr <58414429+maybe-sybr@users.noreply.github.com>
2020-09-22 09:47:43 +1000

    improv: Load all submodules in `medallion.backends`
    This will help us remove the need for importing modules manually when
    instantiating a backend in future changes.

---
+++ medallion/backends/__init__.py
@@ -0,0 +1,10 @@
+import importlib
+import pkgutil
+import sys
+
+# Walk this package and import all sub-modules so the can instantiate backends
+_paths = sys.modules[__name__].__path__
+for _, subname, _ in pkgutil.walk_packages(_paths):
+    mod_obj = importlib.import_module( __name__ + "." + subname)
+    if "." not in subname:
+        globals()[subname] = mod_obj
